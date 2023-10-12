1ca60b54bfdd428cdb8acf066911fb1b329d81b1
FabienCH <fabien.charvaz@protonmail.com>
2023-10-12 08:53:58 +0200

    refactor(species): move homepage to dashboard

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -1,6 +1,6 @@
 import { SpeciesList } from '@/infrastructure/species/species-list';
 
-export default function Home() {
+export default function Dashboard() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <SpeciesList />
